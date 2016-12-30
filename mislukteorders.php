2e3616aeddb93b51a95277556c5f22deb1d491ee
miner66 <b.kroeze@xmsnet.nl>
2016-12-30 14:34:43 +0100

    Add files via upload

--- mislukteorders.php
+++ mislukteorders.php
@@ -16,55 +16,6 @@
 			$('#resultTable').DataTable();
 		} );
 	</script>
-	<?php
-			//vul de query in
-			$queryResult2 = getQuery(
-					"SELECT klant.klantid, klant.naam, 
-						CASE
-							WHEN jaaromzet.jaaromzet IS NULL THEN '0'
-							ELSE jaaromzet
-						END AS 'jaaromzet'
-						,
-						CASE
-							WHEN jaaromzet.jaaromzet < 10000 THEN '5'
-							WHEN jaaromzet.jaaromzet >= 10000 AND jaaromzet.jaaromzet<20000 THEN '10'
-							WHEN jaaromzet.jaaromzet >= 20000 THEN '15'
-							ELSE '0'
-						END AS percentage
-					FROM jaaromzet
-					RIGHT JOIN klant ON klant.klantid=jaaromzet.klantid;"
-			);
-		?>
-	<!-- amCharts javascript code -->
-	<script src="amcharts/amcharts.js" type="text/javascript"></script>
-	<script src="amcharts/pie.js" type="text/javascript"></script>
-	<script type="text/javascript">
-		AmCharts.makeChart("chartdiv",
-			{
-				"type": "pie",
-				"angle": 12,
-				"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
-				"depth3D": 15,
-				"innerRadius": 0,
-				"titleField": "Apotheker",
-				"valueField": "Jaaromzet",
-				"allLabels": [],
-				"balloon": {},
-				"legend": {
-					"enabled": true,
-					"align": "center",
-					"markerType": "circle"
-				},
-				"titles": [],
-				"dataProvider": [
-					<?php while($row2 = $queryResult2->fetch_assoc()): ?>{
-						"Apotheker": "<?php echo $row2['naam'];?>",
-						"Jaaromzet": <?php echo $row2['jaaromzet'];?>
-					},<?php endwhile;?>
-				]
-			}
-		);
-	</script>
 </head>
 <body>
 	<header> 
@@ -76,20 +27,12 @@
 		<?php
 			//vul de query in
 			$queryResult1 = getQuery(
-					"SELECT klant.klantid, klant.naam, 
-						CASE
-							WHEN jaaromzet.jaaromzet IS NULL THEN '0'
-							ELSE jaaromzet
-						END AS 'jaaromzet'
-						,
-						CASE
-							WHEN jaaromzet.jaaromzet < 10000 THEN '5'
-							WHEN jaaromzet.jaaromzet >= 10000 AND jaaromzet.jaaromzet<20000 THEN '10'
-							WHEN jaaromzet.jaaromzet >= 20000 THEN '15'
-							ELSE '0'
-						END AS percentage
-					FROM jaaromzet
-					RIGHT JOIN klant ON klant.klantid=jaaromzet.klantid;"
+					"SELECT orderklant.orderid, klant.naam AS 'klantnaam', medewerkers.naam AS 'medewerkernaam', datumorder, `status`
+					FROM orderklant
+					JOIN klant ON klant.klantid=orderklant.klantid
+					JOIN medewerkers ON medewerkers.medewerkerid=orderklant.medewerkerid
+					WHERE orderklant.backorder IS NOT NULL
+					AND DATEDIFF(DATE(NOW()),DATE(orderklant.datumorder))<183;"
 			);
 		?>
 		
@@ -98,25 +41,25 @@
 		<table id='resultTable'>
 			<thead>
 				<tr>
-					<td>ID</td>
-					<td>Naam</td>
-					<td>Jaaromzet</td>
-					<td>Korting</td>
+					<td>OrderID</td>
+					<td>Naam klant</td>
+					<td>Naam medewerker</td>
+					<td>Datum van order</td>
+					<td>Status order</td>
 				</tr>
 			</thead>
 			<tbody>
 				<?php   while($row1 = $queryResult1->fetch_assoc()): ?>
 				<tr>
-					<td><?php echo $row1['klantid']; ?></td>
-					<td><?php echo $row1['naam']; ?></td>
-					<td><?php echo $row1['jaaromzet']; ?></td>
-					<td><?php echo $row1['percentage']; ?></td>
+					<td><?php echo $row1['orderid']; ?></td>
+					<td><?php echo $row1['klantnaam']; ?></td>
+					<td><?php echo $row1['medewerkernaam']; ?></td>
+					<td><?php echo $row1['datumorder']; ?></td>
+					<td><?php echo $row1['status']; ?></td>
 				</tr>
 				<?php endwhile;?>
 			</tbody>
 		</table>
-		<hr>
-		<div id="chartdiv" style="width:100%; height:400px;"></div>
 	</div>
 </body>
 </html>

4c454bf6704d5653d9303d136e52699890ed8cd8
miner66 <b.kroeze@xmsnet.nl>
2016-12-30 14:02:44 +0100

    Add files via upload

--- inkooptotaalleveranciers.php
+++ inkooptotaalleveranciers.php
@@ -18,14 +18,16 @@
 	</script>
 	<?php
 			//vul de query in
-			$queryResult2 = getQuery("
-				SELECT product.productid, product.naam, SUM(orderproducten.hoeveelheid) AS 'aantal'
-				FROM orderproducten
-				JOIN product ON orderproducten.productid=product.productid
-				WHERE orderproducten.backorder=0
-				GROUP BY product.productid
-				ORDER BY `aantal` DESC
-				LIMIT 5;"
+			$queryResult2 = getQuery(
+					"SELECT fabrikant.naam, SUM(inkoopprijs.prijs*productenbestelling.hoeveelheid) AS 'inkooptotaal leverancier'
+					FROM productenbestelling
+					JOIN product ON product.productid=productenbestelling.productid
+					JOIN inkoopprijs ON inkoopprijs.productid=product.productid
+					JOIN besteld ON besteld.bestellingid=productenbestelling.bestellingid
+					JOIN fabrikant ON fabrikant.fabrikantid=besteld.besteldbij
+					WHERE inkoopprijs.datum<besteld.datum
+					GROUP BY fabrikant.fabrikantid
+					HAVING MAX(inkoopprijs.datum);"
 			);
 		?>
 	<!-- amCharts javascript code -->
@@ -34,7 +36,7 @@
 	<script type="text/javascript">
 		AmCharts.makeChart("chartdiv",
 			{
-					"type": "serial",
+				"type": "serial",
 					"categoryField": "Naam",
 					"startDuration": 1,
 					"categoryAxis": {
@@ -50,14 +52,14 @@
 							"id": "AmGraph-1",
 							"title": "graph 1",
 							"type": "column",
-							"valueField": "Aantal"
+							"valueField": "inkooptotaal"
 						}
 					],
 					"guides": [],
 					"valueAxes": [
 						{
 							"id": "ValueAxis-1",
-							"title": "Hoeveelheid"
+							"title": "Inkooptotaal"
 						}
 					],
 					"allLabels": [],
@@ -66,13 +68,13 @@
 						{
 							"id": "Title-1",
 							"size": 15,
-							"text": "Top 5 verkochte producten"
+							"text": "Inkooptotaal leveranciers"
 						}
 					],
 					"dataProvider": [
 					<?php while($row2 = $queryResult2->fetch_assoc()): ?>{
 						"Naam": "<?php echo $row2['naam'];?>",
-						"Aantal": <?php echo $row2['aantal'];?>
+						"Inkooptotaal": <?php echo $row2['inkooptotaal leveranciers'];?>
 					},<?php endwhile;?>
 				]
 			}
@@ -89,13 +91,20 @@
 		<?php
 			//vul de query in
 			$queryResult1 = getQuery(
-				"SELECT product.productid, product.naam, SUM(orderproducten.hoeveelheid) AS 'aantal'
-				FROM orderproducten
-				JOIN product ON orderproducten.productid=product.productid
-				WHERE orderproducten.backorder=0
-				GROUP BY product.productid
-				ORDER BY `aantal` DESC
-				LIMIT 5;"
+					"SELECT klant.klantid, klant.naam, 
+						CASE
+							WHEN jaaromzet.jaaromzet IS NULL THEN '0'
+							ELSE jaaromzet
+						END AS 'jaaromzet'
+						,
+						CASE
+							WHEN jaaromzet.jaaromzet < 10000 THEN '5'
+							WHEN jaaromzet.jaaromzet >= 10000 AND jaaromzet.jaaromzet<20000 THEN '10'
+							WHEN jaaromzet.jaaromzet >= 20000 THEN '15'
+							ELSE '0'
+						END AS percentage
+					FROM jaaromzet
+					RIGHT JOIN klant ON klant.klantid=jaaromzet.klantid;"
 			);
 		?>
 		
@@ -106,15 +115,17 @@
 				<tr>
 					<td>ID</td>
 					<td>Naam</td>
-					<td>Aantal</td>
+					<td>Jaaromzet</td>
+					<td>Korting</td>
 				</tr>
 			</thead>
 			<tbody>
 				<?php   while($row1 = $queryResult1->fetch_assoc()): ?>
 				<tr>
-					<td><?php echo $row1['productid']; ?></td>
+					<td><?php echo $row1['klantid']; ?></td>
 					<td><?php echo $row1['naam']; ?></td>
-					<td><?php echo $row1['aantal']; ?></td>
+					<td><?php echo $row1['jaaromzet']; ?></td>
+					<td><?php echo $row1['percentage']; ?></td>
 				</tr>
 				<?php endwhile;?>
 			</tbody>

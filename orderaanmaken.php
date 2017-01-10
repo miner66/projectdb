a27fd3367498286eb4a1ca5c5a745b12843df775
miner66 <playerminer66@gmail.com>
2017-01-10 13:01:46 +0100

    Fixed orders

--- orderaanmaken.php
+++ orderaanmaken.php
@@ -8,6 +8,7 @@
 	<?php
 		include "include/functions.php"
 	?>
+	<script 
 	<script type="text/javascript" src="js/jquery-2.2.4.js"></script>
 	<script type="text/javascript" src="js/datatables.js"></script>
 	<script src='js/search.js'></script>
@@ -51,47 +52,65 @@
 		<?php
 			//vul de query in
 			$queryResult1 = getQuery(
-					"SELECT klant.klantid, klant.naam, 
-						CASE
-							WHEN jaaromzet.jaaromzet IS NULL THEN '0'
-							ELSE jaaromzet
-						END AS 'jaaromzet'
-						,
-						CASE
-							WHEN jaaromzet.jaaromzet < 10000 THEN '5'
-							WHEN jaaromzet.jaaromzet >= 10000 AND jaaromzet.jaaromzet<20000 THEN '10'
-							WHEN jaaromzet.jaaromzet >= 20000 THEN '15'
-							ELSE '0'
-						END AS percentage
-					FROM jaaromzet
-					RIGHT JOIN klant ON klant.klantid=jaaromzet.klantid;"
+					"SELECT product.bestelcode, product.naam, product.aantalperverpakking, product.minimaalaantal, prijs.prijs
+					FROM product
+					INNER JOIN
+						(SELECT verkoopprijs.*
+						FROM verkoopprijs
+						INNER JOIN
+							(SELECT productid, MAX(datum) AS MaxDateTime
+							FROM verkoopprijs
+							GROUP BY productid) AS tijd 
+						ON verkoopprijs.productid= tijd.productid
+						AND verkoopprijs.datum = tijd.MaxDateTime) AS prijs
+					ON product.productid=prijs.productid
+					WHERE inverkoop=1;"
 			);
 		?>
 		
 		<!-- maak er een table van -->
-		<!--<input type="text" id="searchInput" onkeyup="searchFunction1()" placeholder="Typ de naam...">-->
 		<table id='resultTable'>
 			<thead>
 				<tr>
-					<td>ID</td>
-					<td>Naam</td>
-					<td>Jaaromzet</td>
-					<td>Korting</td>
+					<th>Bestelcode</th>
+					<th>Naam product</th>
+					<th>Aantal per verpakking</th>
+					<th>Minimale hoeveelheid</th>
+					<th>Hoeveelheid</th>
+					<th>Prijs</th>
+					<th>Totaalprijs</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php   while($row1 = $queryResult1->fetch_assoc()): ?>
 				<tr>
-					<td><?php echo $row1['klantid']; ?></td>
-					<td><?php echo $row1['naam']; ?></td>
-					<td><?php echo $row1['jaaromzet']; ?></td>
-					<td><?php echo $row1['percentage']; ?></td>
+					<td><span name='bestelCode'><?php echo $row1['bestelcode']; ?></span></td>
+					<td><span name='prodNaam'>Conce<?php echo $row1['naam']; ?>rta</span></td>
+					<td><?php echo $row1['aantalperverpakking']; ?></td>
+					<td><span name='minNum'><?php echo $row1['minimaalaantal']; ?></span></td>
+					<td><input name="numArtikel" type="number" maxlength="3" class="amountField" placeholder='0' value='0' onkeyup="doCalc()"></td>
+					<td><span>&euro;</span><span name='linePrice'><?php echo $row1['prijs']; ?></span></td>
+					<td><span>&euro;</span><span name='lineCost'>0.00</span></td>
 				</tr>
 				<?php endwhile;?>
 			</tbody>
 		</table>
+				
 		<hr>
-		<div id="chartdiv" style="width:100%; height:400px;"></div>
+		
+		<table>
+			<thead>
+				<tr>
+					<th>Bestelcode</th>
+					<th>Naam</th>
+					<th>Hoeveelheid</th>
+					<th>Totaalprijs</th>
+				</tr>
+			</thead>
+			<tbody id='orderLines'>
+				
+			</tbody>
+		</table>
 	</div>
 </body>
 </html>
